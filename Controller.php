4e7eb987b1357451b08ea6d755edf3afd9650259
lzc <861260447@qq.com>
2014-11-21 18:08:33 +0800

    添加生成应用脚本

--- Controller.php
+++ Controller.php
@@ -7,10 +7,6 @@ class Controller extends CController
 	/* 默认的函数:action为空时默认为执行此函数 */	
 	public function index(){
 		$this->layout = 'leftSide';
-		$model = Model::get('tab')->find();
-		$result = new Model();
-		$result->setTable('tab');
-		$result->findAll('id!=1');
 		$this->render('index',array('i'=>1));
 	}
 	
